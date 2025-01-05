78c210f03619204c251733d5d7ddaefad2e215c2
Pavlo Chudin <maks.chudin567@gmail.com>
2025-01-06 01:18:38 +0200

    Updates

---
+++ assets/Accessories-C2kaD9pY.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C9EK3TRi.js";import{C as o}from"./CategoryPage-FTN-G7x7.js";import"./Cart-8Lp5QBqz.js";const m=()=>r.jsx(o,{});export{m as Accessories};
---
+++ assets/Tablets-CeUjjlMW.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C9EK3TRi.js";import{C as t}from"./CategoryPage-FTN-G7x7.js";import"./Cart-8Lp5QBqz.js";const m=()=>r.jsx(t,{});export{m as Tablets};
