13b03b75fa2a7b290c10d910a775d64c049fd657
Pavlo Chudin <maks.chudin567@gmail.com>
2025-01-08 22:54:57 +0200

    Updates

--- assets/Phones-BkYDkHVP.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-BN7vwC4w.js";import{C as r}from"./CategoryPage-BrzMkutr.js";import"./Cart-C_iCHzcd.js";const m=()=>o.jsx(r,{});export{m as Phones};
---
+++ assets/Phones-D1XSq-vN.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BjKfoqoD.js";import{C as r}from"./CategoryPage-i2KO9tvo.js";import"./Cart-BExJTyYg.js";const m=()=>o.jsx(r,{});export{m as Phones};
