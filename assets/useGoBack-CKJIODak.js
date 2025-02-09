5788fc4585bb2013d7b00410460e033af3413e2b
Pavlo Chudin <maks.chudin567@gmail.com>
2025-02-09 02:39:09 +0200

    Updates

--- assets/useGoBack-CKJIODak.js
+++
@@ -1,1 +0,0 @@
-import{d as o,r as t}from"./index-7A1rGBmD.js";const e=()=>{const a=o();return{goBack:t.useCallback(()=>{a({pathname:".."})},[a])}};export{e as u};
---
+++ assets/useGoBack-Dhcgsfee.js
@@ -0,0 +1,1 @@
+import{d as o,r as t}from"./index-6ZDlZQzV.js";const e=()=>{const a=o();return{goBack:t.useCallback(()=>{a({pathname:".."})},[a])}};export{e as u};
